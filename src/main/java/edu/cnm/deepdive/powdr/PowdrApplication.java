0ffc7420338cd908242ca7848b675a661071a1d3
anayadrian1 <anayadrian1@gmail.com>
2020-10-22 13:29:47 -0600

    Setting up project.

---
+++ src/main/java/edu/cnm/deepdive/powdr/PowdrApplication.java
@@ -0,0 +1,13 @@
+package edu.cnm.deepdive.powdr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PowdrApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(PowdrApplication.class, args);
+  }
+
+}
---
+++ src/test/java/edu/cnm/deepdive/powdr/PowdrApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.cnm.deepdive.powdr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PowdrApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
