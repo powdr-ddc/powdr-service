1caa6eb58dc4cfc4d6d58784b3788b792b45e6db
Jonah Rodriguez <jonahrodriguez95@gmail.com>
2020-10-26 20:11:40 -0600

    Implemented Message & Trip entities.

--- src/main/java/edu/cnm/deepdive/powdr/model/entity/Trip.java
+++ src/main/java/edu/cnm/deepdive/powdr/model/entity/Trip.java
@@ -1,11 +1,80 @@
 package edu.cnm.deepdive.powdr.model.entity;
 
+import java.util.Date;
+import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import javax.persistence.Index;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import org.hibernate.annotations.CreationTimestamp;
+import org.springframework.lang.NonNull;
 
+@SuppressWarnings("JpaDataSourceORMInspection")
 @Entity
+@Table(
+    indexes = {
+        @Index(columnList = "start_time"),
+        @Index(columnList = "end_time"),
+        @Index(columnList = "distance")
+    }
+)
 public class Trip {
 
+  @Id
+  @GeneratedValue(strategy = GenerationType.AUTO)
+  @Column(nullable = false, updatable = false)
+  private Long tripId;
+
+  @NonNull
+  @ManyToOne(fetch = FetchType.EAGER)
+  @JoinColumn(name = "user_id", nullable = false, updatable = false)
+  private User user;
+
+  @NonNull
+  @CreationTimestamp
+  @Temporal(TemporalType.TIMESTAMP)
+  private Date start;
+
+  @NonNull
+  @Temporal(TemporalType.TIMESTAMP)
+  private Date end;
+
+  @NonNull
+  @Column(nullable = false)
+  private float distance;
+
+  public Long getTripId() {
+    return tripId;
+  }
+
+  @NonNull
+  public User getUser() {
+    return user;
+  }
+
+  public void setUser(@NonNull User user) {
+    this.user = user;
+  }
+
+  @NonNull
+  public Date getStart() {
+    return start;
+  }
+
+  @NonNull
+  public Date getEnd() {
+    return end;
+  }
+
+  public float getDistance() {
+    return distance;
+  }
 
 }
