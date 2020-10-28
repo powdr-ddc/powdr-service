ee835d77e977323f8fee3d32055c2b6d253405b5
anayadrian1 <anayadrian1@gmail.com>
2020-10-27 20:05:35 -0600

    Added FavoriteMountain entity.

--- src/main/java/edu/cnm/deepdive/powdr/model/entity/FavoriteMountain.java
+++ src/main/java/edu/cnm/deepdive/powdr/model/entity/FavoriteMountain.java
@@ -1,8 +1,58 @@
 package edu.cnm.deepdive.powdr.model.entity;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+import javax.persistence.UniqueConstraint;
+import org.springframework.lang.NonNull;
 
-//@Entity
+@SuppressWarnings("JpaDataSourceORMInspection")
+@Entity
+@Table(
+    uniqueConstraints =
+        @UniqueConstraint(columnNames = {"user_id", "mountain_id"})
+)
 public class FavoriteMountain {
 
+  @Id
+  @GeneratedValue(strategy = GenerationType.AUTO)
+  @Column(name = "favorite_mountain_id", nullable = false, updatable = false)
+  private Long id;
+
+  @NonNull
+  @ManyToOne
+  @JoinColumn(name = "user_id", nullable = false, updatable = false)
+  private User user;
+
+  @NonNull
+  @ManyToOne
+  @JoinColumn(name = "mountain_id", nullable = false, updatable = false)
+  private Mountain mountain;
+
+  public Long getId() {
+    return id;
+  }
+
+  @NonNull
+  public User getUser() {
+    return user;
+  }
+
+  public void setUser(@NonNull User user) {
+    this.user = user;
+  }
+
+  @NonNull
+  public Mountain getMountain() {
+    return mountain;
+  }
+
+  public void setMountain(@NonNull Mountain mountain) {
+    this.mountain = mountain;
+  }
 }
