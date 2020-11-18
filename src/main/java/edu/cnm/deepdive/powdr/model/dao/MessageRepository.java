2a525778b67b04a7ad7396bc91c37dc3d58b936c
Jonah Rodriguez <jonahrodriguez95@gmail.com>
2020-11-17 17:12:21 -0700

    Added additional queries to repositories.

--- src/main/java/edu/cnm/deepdive/powdr/model/dao/MessageRepository.java
+++ src/main/java/edu/cnm/deepdive/powdr/model/dao/MessageRepository.java
@@ -1,10 +1,13 @@
 package edu.cnm.deepdive.powdr.model.dao;
 
 import edu.cnm.deepdive.powdr.model.entity.Message;
+import edu.cnm.deepdive.powdr.model.entity.User;
+import java.util.List;
 import java.util.UUID;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface MessageRepository extends JpaRepository<Message, UUID> {
 
 
+  List<Message> getAllBySenderOrReceiver(User sender, User receiver);
 }
